4992ebe10650e37cec1140b950c9959e07807554
Ben Chatelain <ben@octop.ad>
2015-05-06 17:54:23 -0600

    Add folder skipt list to objective_clean.sh

--- objective_clean.sh
+++ objective_clean.sh
@@ -12,7 +12,7 @@ fi
 
 if [[ -z ${SKIP_OBJCLEAN} || ${SKIP_OBJCLEAN} != 1 ]]; then
     if [[ -d "${LOCAL_APPS_DIR}/Objective-Clean.app" ]]; then
-        "${LOCAL_APPS_DIR}"/Objective-Clean.app/Contents/Resources/ObjClean.app/Contents/MacOS/ObjClean "${SRCROOT}"
+        "${LOCAL_APPS_DIR}"/Objective-Clean.app/Contents/Resources/ObjClean.app/Contents/MacOS/ObjClean "${SRCROOT}"?!build,!Carthage,!Frameworks,!Vendor
     else
         echo "warning: You have to install and set up Objective-Clean to use its features!"
     fi
